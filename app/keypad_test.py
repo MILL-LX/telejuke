e153454677e13f0c76b51b4fd24c677f355d54d1
Mo Mobarak <mo@mobarak.com>
2025-06-13 15:49:42 +0100

    respect off-hook switch

--- app/keypad_test.py
+++ app/keypad_test.py
@@ -2,8 +2,8 @@ import lgpio
 import time
 
 # BCM GPIO pin numbers for rows and columns
-ROWS = [16, 6, 13, 19]
-COLS = [26, 20, 21]
+ROW_PINS = [16, 6, 13, 19]
+COLUMN_PINS = [26, 20, 21]
 
 KEY_MAP = [
     ["1", "2", "3"],
@@ -12,39 +12,51 @@ KEY_MAP = [
     ["*", "0", "#"]
 ]
 
+# BCM GPIO pin for off hook switch
+OFF_HOOK_PIN = 12
+
 def setup_pins(h):
     # Set rows as outputs, columns as inputs with pull-downs
-    for row in ROWS:
+    for row in ROW_PINS:
         lgpio.gpio_claim_output(h, row, 0)
-    for col in COLS:
+    for col in COLUMN_PINS:
         lgpio.gpio_claim_input(h, col, lgpio.SET_PULL_DOWN)
 
-def getKey(h):
-    while True:
-        for i, row in enumerate(ROWS):
+    # Set off hook pin as input with pull-up
+    lgpio.gpio_claim_input(h, OFF_HOOK_PIN, lgpio.SET_PULL_UP)
+
+def is_off_hook(h):
+    off_hook = 1 == lgpio.gpio_read(h, OFF_HOOK_PIN)
+    return off_hook
+
+def getButton(h):
+    while is_off_hook(h):
+        for i, row in enumerate(ROW_PINS):
             # Set current row high, others low
-            for r in ROWS:
+            for r in ROW_PINS:
                 lgpio.gpio_write(h, r, 1 if r == row else 0)
             time.sleep(0.01)
-            for j, col in enumerate(COLS):
+            for j, col in enumerate(COLUMN_PINS):
                 if lgpio.gpio_read(h, col):
                     # Debounce
                     time.sleep(0.1)
                     if lgpio.gpio_read(h, col):
-                        key = KEY_MAP[i][j]
-                        print(f'Key pressed: {key}')
-                        # Wait for key release
+                        button = KEY_MAP[i][j]
+
                         while lgpio.gpio_read(h, col):
                             time.sleep(0.01)
-                        return key
+                        return button
+    return None
+     
 
 if __name__ == "__main__":
     h = lgpio.gpiochip_open(0)
     setup_pins(h)
     try:
         while True:
-            KeyLucky = getKey(h)
-            print(KeyLucky)
+            if button := getButton(h):
+                print(f"Button pressed: {button}")
+
     except KeyboardInterrupt:
         pass
     finally:
