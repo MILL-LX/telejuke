af912ced90120b5826ca2c3f61d6be23d3806d13
Mo Mobarak <mo@mobarak.com>
2025-06-13 17:57:34 +0100

    start of recognizing country prefixes

--- apps/keypad_monitor/app.py
+++ apps/keypad_monitor/app.py
@@ -1,16 +1,14 @@
 import keypad
 
 def monitor_keypad():
-    """
-    Continuously monitors the keypad for key presses and prints the pressed keys.
-    """
-    print("Starting keypad monitor...")
+    country_prefix = ""
     while True:
-        key = keypad.get_button()
-        if key is not None:
-            print(f"Key pressed: {key}")
+        if keypad.is_off_hook():
+            if key := keypad.get_button():
+                country_prefix += key
         else:
-            print("No key pressed.")
+            country_prefix = ""
+
 
 if __name__ == "__main__":
     keypad.setup()  # Initialize the keypad hardware
